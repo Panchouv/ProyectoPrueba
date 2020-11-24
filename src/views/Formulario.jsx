4afc96c51c1e034edcb1ff0ee36c20f9662892cc
sebastian zelaya <sebastian.zelaya@alumnos.uv.cl>
2020-11-24 13:19:37 -0300

    agregando formulario, agregando vista, componentes

---
+++ src/views/Formulario.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Form } from 'semantic-ui-react'
+import FormularioComponent from '../components/FormularioComponent'
+
+function Formulario() {
+    return (
+        <div className='formulario'> 
+            <h1>Formulario</h1>
+            <FormularioComponent/>
+        </div>
+    )
+}
+
+export default Formulario
